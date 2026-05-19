b937d976cda6b2139e3a015748e18b706a3f52ac
00h50m <31708036+00h50m@users.noreply.github.com>
2026-05-19 20:51:58 -0300

    Update supabase.js

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,14 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://gsixrfvbusezudqbquiu.supabase.co";
-const supabaseUrl = "https://gsixrfvbusezudqbquiu.supabase.co/rest/v1/;
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzaXhyZnZidXNlenVkcWJxdWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NTIxMTEsImV4cCI6MjA4NjMyODExMX0.7TAhXexcqjhfCcL1CDPx1llz46uGIWZkYaW32BiGzTw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
-    persistSession: true,      // mantém sessão no localStorage
+    persistSession: true,
     detectSessionInUrl: false,
-    storageKey: "Santana Method-auth", // chave única no localStorage
+    storageKey: "santana-method-auth",
   },
 });
